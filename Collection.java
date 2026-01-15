910c521d3daea2b75d5b963ba4dda605340f69e3
Luca_Previ0o <luca01.previati@edu.unife.it>
2026-01-15 16:14:01 +0100

    updated Javadoc using Markdown-styled comments

--- Collection.java
+++ Collection.java
@@ -4,46 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-/**
- * Collection of Model instances.
- * This class provides methods to add, select, and remove models based on conditions.
- * It also supports serialization and deserialization of the collection.
- * @param <T> type of Model contained in the collection
- */
+/// Collection of Model instances.
+///
+/// This class provides methods to add, select, and remove models based on conditions.
+/// It also supports serialization and deserialization of the collection.
+/// @param <T> type of Model contained in the collection
 public class Collection<T extends Model> extends Model {
     
     String name;
     List<T> items = new ArrayList<>();
 
-    /**
-     * Constructs a Collection with the specified name.
-     * @param name the name of the collection
-     */
+    /// Constructs a Collection with the specified name.
+    /// @param name the name of the collection
     public Collection(String name) { this.name = name; }
 
-    /**
-     * Returns the name of the collection.
-     * @return the name of the collection
-     */
+    /// Returns the name of the collection.
+    /// @return the name of the collection
     public String getName() { return name; }
 
-    /**
-     * Adds a model to the collection.
-     * @param model the model to add
-     */
+    /// Adds a model to the collection.
+    /// @param model the model to add
     public void add(T model) { items.add(model); }
 
-    /**
-     * Adds multiple models to the collection.
-     * @param models array of models to add
-     */
+    /// Adds multiple models to the collection.
+    /// @param models array of models to add
     public void add(T[] models) { for (var model : models) add(model); }
 
-    /**
-     * Selects models from the collection that satisfy the given condition.
-     * @param condition the predicate condition to filter models
-     * @return list of models that satisfy the condition
-     */
+    /// Selects models from the collection that satisfy the given condition.
+    /// @param condition the predicate condition to filter models
+    /// @return list of models that satisfy the condition
     public List<T> select(Predicate<T> condition) {
         
         var results = new ArrayList<T>();
@@ -52,16 +41,12 @@ public class Collection<T extends Model> extends Model {
         return results;
     }
 
-    /**
-     * Selects all models from the collection.
-     * @return list of all models in the collection
-     */
+    /// Selects all models from the collection.
+    /// @return list of all models in the collection
     public List<T> select() { return items; }
 
-    /**
-     * Removes models from the collection that satisfy the given condition.
-     * @param condition the predicate condition to filter models for removal
-     */
+    /// Removes models from the collection that satisfy the given condition.
+    /// @param condition the predicate condition to filter models for removal
     public void remove(Predicate<T> condition) { items.removeIf(condition); }
 
     @Override
--- Model.java
+++ Model.java
@@ -1,20 +1,16 @@
 package jdbi;
 
-/**
- * Abstract base class for all models in the JDBI framework.
- */
+/// Abstract base class for all models in the JDBI framework.
 public abstract class Model {
     
-    /**
-     * Serialize the model into a storable line.
-     * @return The serialized representation of the model.
-     */
+    /// Serialize the model into a storable line.
+    ///
+    /// The serialization format is implementation-specific (e.g., CSV, JSON).
+    /// @return The serialized representation of the model.
     public abstract String serialize();
 
-    /**
-     * De-serialize the given data into a model instance.
-     * @param data The serialized data.
-     * @return The deserialized model instance.
-     */
+    /// De-serialize the given data into a model instance.
+    /// @param data The serialized data.
+    /// @return The deserialized model instance.
     public abstract Model deserialize(String data);
 }
