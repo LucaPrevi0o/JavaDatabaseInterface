f96359f3d2574f48394bbe1b6bfdbc801f1eb3bc
Luca_Previ0o <luca01.previati@edu.unife.it>
2026-01-16 08:23:37 +0100

    Resloved conflicts

--- jdbi/ModelFactory.java
+++
@@ -1,9 +0,0 @@
-package jdbi;
-
-public abstract class ModelFactory<T extends Model> {
-    
-    /**
-     * storage engine uses this to create models from serialized data
-     */
-    public abstract T deserialize(String data);
-}
