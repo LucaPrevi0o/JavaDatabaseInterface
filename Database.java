e86a996cb599d9800e797bd62dce48e0f69b578c
Luca_Previ0o <luca01.previati@edu.unife.it>
2026-01-21 17:09:38 +0100

    changed Database class to concrete

--- Database.java
+++ Database.java
@@ -9,31 +9,7 @@ import java.util.function.Consumer;
 /// and perform actions within a transaction context.
 /// The use of a database allows for flexibility in managing different storage backends
 /// while maintaining a consistent interface for model operations.
-public abstract class Database {
-
-    /// Represents a transaction context for performing actions on a specific storage engine.
-    /// @param <M> The type of model managed by the storage engine.
-    /// @param <S> The type of storage engine.
-    public static class Transaction<M extends Model, S extends StorageEngine<M>> {
-
-        private final S engine;
-        private final Consumer<S> actions;
-
-        /// Constructs a Transaction with the specified storage engine and actions.
-        /// @param engine  The storage engine instance for the transaction.
-        /// @param actions A consumer that defines the actions to be performed within the transaction.
-        public Transaction(S engine, Consumer<S> actions) {
-
-            this.engine = engine;
-            this.actions = actions;
-        }
-
-        /// Commits the transaction by executing the defined actions on the storage engine.
-        public void commit() { actions.accept(engine); }
-
-        /// Rolls back the transaction. (No-op in this implementation)
-        public void rollback() { }
-    }
+public class Database {
 
     /// Begin a transaction for the specified model type, executing the provided actions.
     ///
