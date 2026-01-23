5eb378baf20952aec5241a9d0248ae4013b79d8b
Luca_Previ0o <luca01.previati@edu.unife.it>
2026-01-23 17:36:09 +0100

    implemented simple snapshot-based rollback

--- transaction/Transaction.java
+++ transaction/Transaction.java
@@ -6,12 +6,16 @@ import com.lucaprevioo.jdbi.StorageEngine;
 import java.util.List;
 
 /// Represents a transaction context for performing actions on a specific storage engine.
+///
+/// Using a transaction, multiple actions can be executed atomically, ensuring data integrity.
+/// If any action fails, the transaction can be rolled back to restore the previous state.
 /// @param <M> The type of model managed by the storage engine.
 /// @param <S> The type of storage engine.
 public class Transaction<M extends Model, S extends StorageEngine<M>> {
 
     private final S engine;
     private final List<Action<M>> actions;
+    private List<M> snapshot = null;
 
     /// Constructs a Transaction with the specified storage engine and actions.
     /// @param engine  The storage engine instance for the transaction.
@@ -23,12 +27,25 @@ public class Transaction<M extends Model, S extends StorageEngine<M>> {
     }
 
     /// Commits the transaction by executing the defined actions on the storage engine.
+    /// If any action fails, the transaction is rolled back to restore the previous state.
     public void commit() {
 
-        try { for (var action : actions) action.execute(engine); }
-        catch (Exception e) { throw new RuntimeException("Transaction failed: " + e.getMessage(), e); }
+        try {
+
+            snapshot = engine.read();
+            for (var action : actions) action.execute(engine);
+        } catch (Exception e) {
+
+            try { rollback(); }
+            catch (Exception re) { throw new RuntimeException("Rollback failed: " + re.getMessage(), re); }
+        } finally { snapshot = null; }
     }
 
-    /// Rolls back the transaction. (No-op in this implementation)
-    public void rollback() { }
+    /// Rolls back the transaction by restoring the storage engine to its previous state.
+    public void rollback() {
+
+        if (snapshot == null) throw new RuntimeException("No snapshot available for rollback.");
+        engine.delete(_ -> true);
+        engine.create(snapshot);
+    }
 }
